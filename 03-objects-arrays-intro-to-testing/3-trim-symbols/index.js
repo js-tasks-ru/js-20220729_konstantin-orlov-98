a2e2e13c1509b43cb6191dabb218b8493d049916
Kostya Orlov <konstantin_orlov_98@mail.ru>
2022-08-09 15:40:58 +0500

    Homework 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let newStr = '';
+    let promStr = string[0];
 
+    for (let i = 1; i <= string.length; i++) {
+        if(string[i] === promStr[promStr.length - 1]) {
+            promStr += string[i];
+            console.log(promStr)
+        } else {
+            newStr += promStr.slice(0, size);
+            promStr = string[i]
+        }
+    }
+
+    return newStr;
 }
